c31523d12cba25042dcb3331f68ad402e8887bf2
Bhushan Darne <darnebhushan709@gmail.com>
2026-06-28 17:56:26 +0530

    completed JWT Authentication

---
+++ server/models/Event.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    venue: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("🚀 CampusConnect Backend is Running Successfully!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
